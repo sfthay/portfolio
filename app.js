3d8258c4a92c9100663cb475794eb63c2edd3a48
Thaynara Maciel <thay.maciel7@gmail.com>
2022-08-23 23:54:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions(){ 
+    //button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //sections active class
+    allSections.addEventListener('click', (e) =>{
+        //console.log(e.target); utilizado para ver se todos os botões estão indo para o console;
+        const id = e.target.dataset.id; //serve para clicar e buscar os ids;
+        if(id){
+            //remove selected from the other button;
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })    
+}
+
+PageTransitions(); //calling the function;
+
+
+
+
